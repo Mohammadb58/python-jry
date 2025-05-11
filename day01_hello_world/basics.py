2daea278cf7d8c061a6a299e5c06f328ba8b2834
Mohammad Abu Rashed <maburrashed543210@gmail.com>
2025-05-11 16:44:18 -0500

    Day 1 – added practice with all data types (strings, ints, floats, bools)

---
+++ day01_hello_world/basics.py
@@ -0,0 +1,21 @@
+# Strings, Integers, floats, and boolean
+
+first_name = "Mohammad"
+last_name = "Abu Rashed"
+age = 20
+
+age_in_months =  251.5
+
+#is_student = Yes
+#is_student = 1
+is_student = True
+#is_student = T
+
+
+#print(f"Hello {first_name}")
+#print(f"Last name: {last_name}")
+#print (age)
+
+print(f"Hello {first_name} {last_name} you are {age}")
+print(f"Your age in months is: {age_in_months}")
+print(f"Are you a student? {is_student}")
