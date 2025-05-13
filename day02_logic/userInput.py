2bee1df252b93c4cf13def00f35090592de1a8b7
Mohammad Abu Rashed <maburrashed543210@gmail.com>
2025-05-13 09:34:39 -0500

    feat: Day 2

---
+++ day02_logic/userInput.py
@@ -0,0 +1,13 @@
+# userinput
+firstName = input("What is your name? ")
+
+print (f"Hello {firstName}")
+
+age = input("How old are you? ")
+
+print (f"You are {age} years old")
+
+age = int(age)
+age = age + 1
+
+print (f"You are going to be {age} in few months!")
