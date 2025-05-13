19b04f2a7e38d1b8b9301d21d2bd08ddc80e47b3
Mohammad Abu Rashed <maburrashed543210@gmail.com>
2025-05-13 10:12:53 -0500

    update

---
+++ day02_logic/recArea.py
@@ -0,0 +1,11 @@
+# Finding the area of a rec A = w*l
+
+# int would only take whole numbers
+# float would handle both int/float
+width = float(input("What is the Width: "))
+length = float(input("What is the length: "))
+
+area = width * length
+
+print (f"The area of your rec is {area} cm^2.")
+
