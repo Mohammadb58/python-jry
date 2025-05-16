fd2e2a08b6394e5ac387ec67a9cc391318e4ccc5
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-05-15 19:51:12 -0500

    update

---
+++ day04_strings/justPrac.py
@@ -0,0 +1,17 @@
+user = "Mohammad"
+
+access = "Access" if user == "Mohammad" else "Denied"
+
+print(access)
+
+temp = 66
+
+goForAWalk = "Go for a walk" if temp >= 55 else "Stay home"
+
+print(goForAWalk)
+
+age = 17
+
+accessAge = "Access" if age >= 18 else "Denied"
+
+print(accessAge)
