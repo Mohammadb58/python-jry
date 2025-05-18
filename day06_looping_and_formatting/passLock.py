5d7b98b5a57338866be2ce5a6ee24d09436efefa
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-05-18 11:20:22 -0500

    update

--- day06_looping_and_formatting/passLock.py
+++ day06_looping_and_formatting/passLock.py
@@ -6,41 +6,32 @@ userPass1 = input("What do you think the password is: ")
 
 userPass2 = userPass1.strip().lower()
 
-attemptsCount = 1
-if userPass2 == "league":
-    userPassBool = True 
-else:
-    userPassBool = False
+attemptsCount = 1    
+attemptsCountBool = True 
     
-if attemptsCount <= 4:
-    attemptsCountBool = True 
-else:
-    attemptsCountBool = False 
 
-while (userPassBool == False) or (attemptsCountBool == False):
-    if attemptsCount <= 4:
-        if attemptsCount == 1:
-            print("❌ Wrong, You got 3 attempts left")
-            userPass2 = input("What do you think the password is: ")
-            userPass2 = userPass2.strip().lower()
-            attemptsCount += 1
-        elif attemptsCount == 2:
-            print("❌ Wrong, You got 2 attempts left")
-            userPass2 = input("What do you think the password is: ")
-            userPass2 = userPass2.strip().lower()
-            attemptsCount += 1
-        elif attemptsCount == 3:
-            print("❌ Wrong, You got 1 attempt left")
-            userPass2 = input("What do you think the password is: ")
-            userPass2 = userPass2.strip().lower()
-            attemptsCount += 1
-        else:
-            attemptsCountBool = False
-    else:
-        userPassBool = False
+while not (userPass2 == "league") and (attemptsCountBool == True):
+    #if attemptsCount <= 4:
+    if attemptsCount == 1:
+        print("❌ Wrong, You got 3 attempts left")
+        userPass2 = input("What do you think the password is: ")
+        userPass2 = userPass2.strip().lower()
+        attemptsCount = 2
+    elif attemptsCount == 2:
+        print("❌ Wrong, You got 2 attempts left")
+        userPass2 = input("What do you think the password is: ")
+        userPass2 = userPass2.strip().lower()
+        attemptsCount = 3
+    elif attemptsCount == 3:
+        print("❌ Wrong, You got 1 attempt left")
+        userPass2 = input("What do you think the password is: ")
+        userPass2 = userPass2.strip().lower()
+        attemptsCount = 4
+    elif attemptsCount >= 4:
+        attemptsCountBool = False
+        
 
-if (userPassBool == True) and (attemptsCountBool == True):
-    print("X")
+if (userPass2 == "league") and (attemptsCountBool == True):
+    print("✅ ACCESS")
 else:
-    print("X")
-print("✅ ACCESS")
+    print("❌ Access Denied ❌")
