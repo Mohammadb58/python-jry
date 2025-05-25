013d2157c8eb8860d28cf06496bcafc09a13b451
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-05-25 13:02:50 -0500

    update

--- day13_more_nested_loops/moreNestedLoops.py
+++ day13_more_nested_loops/moreNestedLoops.py
@@ -12,4 +12,17 @@ for rows in range(1,6):
         print("A ", end="") if (rows + cols) % 2 ==0  else print("B ", end="")
     print()
 print("-----------------------------")    
-    
+
+counter = 1   
+for i in range(1,6):
+    for j in range(counter,counter+5):
+        #print(j,"",end="")
+        # This did not work because the counter
+        # starts at 1 so it print 5 O's then it
+        # then it starts at 6 so it prints 5 E's
+        # but what we wanted was each num aka 'j'
+        #print("O ", end="") if counter %2 != 0 else print("E ", end="")
+        print("O ", end="") if j %2 != 0 else print("E ", end="")
+    print()
+    counter+=5
+print("-----------------------------")    
