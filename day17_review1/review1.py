6811729fb89a1f88cb54fbcb387aa2803329ea4d
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-05-31 21:20:33 -0500

    update

--- day17_review1/review1.py
+++ day17_review1/review1.py
@@ -1,3 +1,4 @@
+'''
 # split practice 
 userList = 'm o h a m m a d'
 print(userList.split(' '))
@@ -9,3 +10,43 @@ print(userInput.split())
 userInput = input("Enter what ever on your mind: ")
 
 print(userInput.split(' '))
+
+
+# bool testing (typecast)
+
+bool1 = True
+
+print(type(bool1))
+
+bool2 = False
+
+print(type(bool2))
+
+bool3 = 0
+
+print(type(bool3))
+
+bool4 = 1
+
+print(type(bool4))
+
+bool5 = ' '
+
+print(type(bool5))
+
+bool = 1
+print(bool)
+
+bool = 11
+print(bool)
+
+'''
+
+print(bool(''))
+print(bool('False'))
+# the bool is ONLY checking if there is anything here
+print(bool('falsee'))
+print(bool(1))
+print(bool({False}))
+print(bool([]))
+
