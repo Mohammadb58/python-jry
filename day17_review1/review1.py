ad7a215e7e9353fd8171267be098ec6934a91d6a
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-05-31 20:42:29 -0500

    update

---
+++ day17_review1/review1.py
@@ -0,0 +1,11 @@
+# split practice 
+userList = 'm o h a m m a d'
+print(userList.split(' '))
+
+userInput = input("Enter what ever on your mind: ")
+
+print(userInput.split())
+
+userInput = input("Enter what ever on your mind: ")
+
+print(userInput.split(' '))
