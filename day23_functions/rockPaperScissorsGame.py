5f637c6760502499a7165175db7ea7506793d2f3
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-09 22:38:01 -0500

    update

--- day23_functions/rockPaperScissorsGame.py
+++ day23_functions/rockPaperScissorsGame.py
@@ -5,31 +5,33 @@ print("\nWelcome to the rock, paper, scissors game.")
 
 game = ['rock', 'paper', 'scissors']
 
-programChoice = random.choice(game)
-
-while True:
-    userChoice = input("\nWhat is your pick (rock, paper, scissors): ")
-    if userChoice.lower().strip() == programChoice:
-        print(f"\nIt's a TIE the program choose {programChoice}")
-        break
-    elif userChoice.lower().strip() == 'rock' and programChoice == 'paper':
-        print(f"\nYou LOOSE ✊ the program choose {programChoice}")
-        break
-    elif userChoice.lower().strip() == 'rock' and programChoice == 'scissors':
-        print(f"\nYou WIN ✊ the program choose {programChoice}") 
-        break
-    elif userChoice.lower().strip() == 'paper' and programChoice == 'rock':
-        print(f"\nYou WIN 📄 the program choose {programChoice}")
-        break        
-    elif userChoice.lower().strip() == 'paper' and programChoice == 'scissors':
-        print(f"\nYou LOOSE 📄 the program choose {programChoice}")
-        break
-    elif userChoice.lower().strip() == 'scissors' and programChoice == 'paper':
-        print(f"\nYou WIN ✂️ the program choose {programChoice}")
-        break
-    elif userChoice.lower().strip() == 'scissors' and programChoice == 'rock':
-        print(f"\nYou LOOSE ✂️ the program choose {programChoice}")
-        break
+playing = True
+while playing:
+    programChoice = random.choice(game)
+    while True:
+        userChoice = input("\nWhat is your pick (rock, paper, scissors): ")
+        if userChoice not in game:
+            print('\nInvaild choice')
+            continue 
+        if userChoice.lower().strip() == programChoice:
+            print(f"\nIt's a TIE the program choose {programChoice}")
+            break
+        elif userChoice.lower().strip() == 'rock' and programChoice == 'scissors':
+            print(f"\nYou WIN ✊ the program choose {programChoice}") 
+            break
+        elif userChoice.lower().strip() == 'paper' and programChoice == 'rock':
+            print(f"\nYou WIN 📄 the program choose {programChoice}")
+            break        
+        elif userChoice.lower().strip() == 'scissors' and programChoice == 'paper':
+            print(f"\nYou WIN ✂️ the program choose {programChoice}")
+            break
+        else:
+            print(f"\nYou LOOSE the program choose {programChoice}")
+            break
+    playAg = input("\nWould you like to play again (Y/N): ").strip().lower()
+    if playAg == 'yes' or playAg == 'y':
+        playing = True
     else:
-        print('\nInvaild choice')
-        continue
+        playing = False
+    
+print("\nThanks for playing\n")
